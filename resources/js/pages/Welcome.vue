46dccf62589c3cc5295c4b06c86ba44c741077f9
Eren <yasar451278@gmail.com>
2026-04-18 17:29:18 +0300

    Update Welcome.vue

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -20,6 +20,6 @@ withDefaults(
     <div
         class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]"
     >
-        YAŞAR
+        YAŞAR DENEME
     </div>
 </template>
